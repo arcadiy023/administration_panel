edb86fd80a47e2b1efdead0a280a00bf46a92332
Arcadiy Pochumban <arcadiy>
2023-11-08 00:18:17 +0300

    настройка по дефолту компонента Box

--- src/components/box/index.js
+++ src/components/box/index.js
@@ -2,6 +2,19 @@ import { Box } from "@mui/material";
 
 const Default =(props)=>{
     const{defFlex, ai, jc, center,row, gap, grow, ...other} = props
-    return <Box  {...other}></Box>
+
+    const newSx={};
+    if(defFlex) {
+        newSx.display = "flex";
+        newSx.flexDirection = "column"
+    }
+
+    if (ai) {
+        newSx.alignItems= ai ===true ? "center":ai
+    }
+
+
+
+    return <Box sx={{...newSx,...sx}} {...other}></Box>
 }
 export {Default as Box}
